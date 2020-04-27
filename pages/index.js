07f5fcb68259eee4631591270c6532c58b9a5eb5
jepidoptera <jepidoptera@live.com>
2020-04-27 10:43:54 -0500

    very very basic React page

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,12 @@
 import React from 'react'
+class Index extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>the index page</h1>
+            </div>
+        )
+    }
+}
+
+export default Index;
